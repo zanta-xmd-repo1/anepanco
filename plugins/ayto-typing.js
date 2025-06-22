4399a1de71b0caa6bce1bd118a6667c3504a39ee
zanta-xmd-repo1 <zantaxmdpairweb@gmail.com>
2025-06-22 23:53:53 +0530

    Update ayto-typing.js

--- plugins/ayto-typing.js
+++ plugins/ayto-typing.js
@@ -12,7 +12,7 @@ async (conn, mek, m, { from, body, isOwner }) => {
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
+            //const config = await readEnv();
             if (config.AUTO_REPLY === 'true') {
                 if (isOwner) return;        
                 await m.reply(data[text])
