aaf58acd66c7eca0eb1ee9e815be78c70bd2fa64
zanta-xmd-repo1 <zantaxmdpairweb@gmail.com>
2025-06-22 22:45:08 +0530

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -39,6 +39,7 @@ else BOTOW = "*You are not bot\'s owner or moderator !*"
 //auto reply 
 cmd({
   on: "body"
+
 },    
 async (conn, mek, m, { from, body, isOwner }) => {
     const filePath = path.join(__dirname, '../media/autoreply.json');
@@ -54,27 +55,6 @@ async (conn, mek, m, { from, body, isOwner }) => {
         }
     }                
 });
-
-
-
-//auto reply 
-cmd({
-  on: "body"
-},    
-async (conn, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../media/autoreply.json');
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-    for (const text in data) {
-        if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
-            if (config.AUTO_REPLY === 'true') {
-                if (isOwner) return;        
-                await m.reply(data[text])
-            
-            }
-        }
-    }                
-})
 
 cmd({
     pattern: "movie",
