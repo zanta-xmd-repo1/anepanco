d77d70fba663addfe4f0b40aecd2d372e5e2b1a6
zanta-xmd-repo1 <zantaxmdpairweb@gmail.com>
2025-06-22 22:34:47 +0530

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -55,29 +55,24 @@ async (conn, mek, m, { from, body, isOwner }) => {
     }                
 });
 
-const fs = require('fs');
-const path = require('path');
-var { get_set , input_set } = require('../lib/set_db')
-const {cmd , commands} = require('../command')
-
-//auto_voice
+//auto reply 
 cmd({
   on: "body"
 },    
 async (conn, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../my_data/autovoice.json');
+    const filePath = path.join(__dirname, '../media/autoreply.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
-            var { get_set , input_set } = require('../lib/set_db')
-            if (config.AUTO_VOICE === 'true') {
+            const config = await readEnv();
+            if (config.AUTO_REPLY === 'true') {
                 if (isOwner) return;        
-                await conn.sendPresenceUpdate('recording', from);
-                await conn.sendMessage(from, { audio: { url: data[text] }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+                await m.reply(data[text])
+            
             }
         }
     }                
-});
+})
 
 cmd({
     pattern: "movie",
