aa4ef9dd33ef09a5393e5590d92135da08caf207
zanta-xmd-repo1 <zantaxmdpairweb@gmail.com>
2025-06-22 22:22:57 +0530

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -55,39 +55,25 @@ async (conn, mek, m, { from, body, isOwner }) => {
     }                
 });
 
-//auto sticker 
-cmd({
-  on: "body"
-},    
-async (conn, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../my_data/autosticker.json');
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-    for (const text in data) {
-        if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
-            if (config.AUTO_STICKER === 'true') {
-                //if (isOwner) return;        
-                await conn.sendMessage(from,{sticker: { url : data[text]},package: 'QUEEN-RASHU-MD'},{ quoted: mek })   
-            
-            }
-        }
-    }                
-});
+const fs = require('fs');
+const path = require('path');
+var { get_set , input_set } = require('../lib/set_db')
+const {cmd , commands} = require('../command')
 
-//auto reply 
+//auto_voice
 cmd({
   on: "body"
 },    
 async (conn, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../my_data/autoreply.json');
+    const filePath = path.join(__dirname, '../my_data/autovoice.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
-            if (config.AUTO_REPLY === 'true') {
-                //if (isOwner) return;        
-                await m.reply(data[text])
-            
+            var { get_set , input_set } = require('../lib/set_db')
+            if (config.AUTO_VOICE === 'true') {
+                if (isOwner) return;        
+                await conn.sendPresenceUpdate('recording', from);
+                await conn.sendMessage(from, { audio: { url: data[text] }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
             }
         }
     }                
