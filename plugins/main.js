5320eacff0c3921ca388d28f5ff8ec20f05b5fe5
zanta-xmd-repo1 <zantaxmdpairweb@gmail.com>
2025-06-22 12:55:56 +0530

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -1,18 +1,8 @@
 const config = require('../settings')
 const os = require('os')
 const fs = require('fs')
-const si = require('systeminformation')
 const prefix = config.PREFIX
-const simpleGit = require('simple-git')
-const Levels = require("discord-xp")
-const git = simpleGit()
-const Heroku = require('heroku-client')
-const appname = process.env.APP_NAME || ''
-const herokuapi = process.env.HEROKU_API
-const pingSt = new Date();
 const { cmd, commands } = require('../lib/command')
-const DB = require('../lib/scraper')
-const owner = JSON.parse(fs.readFileSync('./lib/owner.json'))
 const devlopernumber = "94711453361"
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson,clockString, jsonformat} = require('../lib/functions')
 var { updateCMDStore,isbtnID,getCMDStore,getCmdForCmdId,connectdb,input,get, updb,updfb } = require("../lib/database")
@@ -40,6 +30,7 @@ const {
 const reportedMessages = {}
 //const isBan = banUser.includes(mek.sender)
 	    
+
 	
 var BOTOW = ''
 if(config.LANG === 'SI') BOTOW = "*ඔබ Bot\'s හිමිකරු හෝ  උපපරිපාලක නොවේ !*"
