c269c3b3baccab13a81edfc76b6b0d015656a767
zanta-xmd-repo1 <zantaxmdpairweb@gmail.com>
2025-06-22 20:14:17 +0530

    Update ayto-typing.js

--- plugins/ayto-typing.js
+++ plugins/ayto-typing.js
@@ -12,7 +12,7 @@ async (conn, mek, m, { from, body, isOwner }) => {
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
+            var { get_set , input_set } = require('../lib/set_db')
             if (config.AUTO_VOICE === 'true') {
                 if (isOwner) return;        
                 await conn.sendPresenceUpdate('recording', from);
@@ -21,41 +21,3 @@ async (conn, mek, m, { from, body, isOwner }) => {
         }
     }                
 });
-
-//auto sticker 
-/*cmd({
-  on: "body"
-},    
-async (conn, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../media/autosticker.json');
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-    for (const text in data) {
-        if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
-            if (config.AUTO_STICKER === 'true') {
-                if (isOwner) return;        
-                await conn.sendMessage(from,{sticker: { url : data[text]},package: 'yourName'},{ quoted: mek })   
-            
-            }
-        }
-    }                
-});
-
-//auto reply 
-cmd({
-  on: "body"
-},    
-async (conn, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../media/autoreply.json');
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-    for (const text in data) {
-        if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
-            if (config.AUTO_REPLY === 'true') {
-                if (isOwner) return;        
-                await m.reply(data[text])
-            
-            }
-        }
-    }                
-});*/
