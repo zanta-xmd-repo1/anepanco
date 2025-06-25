16ddeb511c3a1957ef004a136f64be2830d7719b
zanta-xmd-repo1 <zantaxmdpairweb@gmail.com>
2025-06-25 13:23:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -516,7 +516,341 @@ conn.ev.on("call", async(json) => {
     	}}
     }); 
 	
+
 //==================================Welcome================================
+	
+
+conn.forwardMessage = async (jid, message, forceForward = false, options = {}) => {
+            let vtype
+            if (options.readViewOnce) {
+                message.message = message.message && message.message.ephemeralMessage && message.message.ephemeralMessage.message ? message.message.ephemeralMessage.message : (message.message || undefined)
+                vtype = Object.keys(message.message.viewOnceMessage.message)[0]
+                delete (message.message && message.message.ignore ? message.message.ignore : (message.message || undefined))
+                delete message.message.viewOnceMessage.message[vtype].viewOnce
+                message.message = {
+                    ...message.message.viewOnceMessage.message
+                }
+            }
+
+            let mtype = Object.keys(message.message)[0]
+            let content = await generateForwardMessageContent(message, forceForward)
+            let ctype = Object.keys(content)[0]
+            let context = {}
+            if (mtype != "conversation") context = message.message[mtype].contextInfo
+            content[ctype].contextInfo = {
+                ...context,
+                ...content[ctype].contextInfo
+            }
+            const waMessage = await generateWAMessageFromContent(jid, content, options ? {
+                ...content[ctype],
+                ...options,
+                ...(options.contextInfo ? {
+                    contextInfo: {
+                        ...content[ctype].contextInfo,
+                        ...options.contextInfo
+                    }
+                } : {})
+            } : {})
+            await conn.relayMessage(jid, waMessage.message, { messageId: waMessage.key.id })
+            return waMessage
+             }
+
+
+
+	
+
+
+	
+//==================================================================
+
+
+	
+// respon cmd pollMessage
+async function getMessage(key) {
+    if (store) {
+        const msg = await store.loadMessage(key.remoteJid, key.id);
+        return msg?.message;
+    }
+    return {
+        conversation: "Hai",
+    };
+}
+
+conn.ev.on('messages.update', async chatUpdate => {
+    for (const { key, update } of chatUpdate) {
+        if (update.pollUpdates && key.fromMe) {
+            const pollCreation = await getMessage(key);
+            if (pollCreation) {
+                const pollUpdate = await getAggregateVotesInPollMessage({
+                    message: pollCreation,
+                    pollUpdates: update.pollUpdates,
+                });
+                var toCmd = pollUpdate.filter(v => v.voters.length !== 0)[0]?.name;
+                if (toCmd == undefined) return;
+                var prefCmd = prefix + toCmd;
+
+                try {
+                    setTimeout(async () => {
+                        await gss.sendMessage(key.remoteJid, { delete: key });
+                    }, 10000);
+                } catch (error) {
+                    console.error("Error deleting message:", error);
+                }
+
+                gss.appenTextMessage(prefCmd, chatUpdate);
+            }
+        }
+    }
+});
+
+
+
+conn.ev.on('messages.update', async(mes) => {
+        for(const { key, update } of mes) {
+            if(update.pollUpdates) {
+                const pollCreationmg = await getMessage(key)
+                const pollCreation = pollCreationmg.message;
+                if(pollCreation) {
+                    const from = key.remoteJid;
+                    const botNumber = await jidNormalizedUser(conn.user.id);
+                    const pollMessage = await getAggregateVotesInPollMessage({
+                        message: pollCreation,
+                        pollUpdates: update.pollUpdates,
+                    })
+                    let bodyName = pollMessage.find(poll => poll.voters.length > 0)?.name || '';
+                    let bodyIndex = pollMessage.findIndex(poll => poll.name === bodyName) || '';
+                    
+                    let voter = (pollMessage.find(poll => poll.voters.length > 0)?.voters[0] == 'me')?botNumber  :pollMessage.find(poll => poll.voters.length > 0)?.voters[0];
+                    function extractMentionedJid(data) {
+                        let messageKeys = ['pollCreationMessage', 'pollCreationMessageV1', 'pollCreationMessageV2', 'pollCreationMessageV3'];
+                    
+                        for (let key of messageKeys) {
+                            if (data[key]  && data[key].mentionedJid) {
+                                return data[key].mentionedJid;
+                            }
+                        }
+                    
+                        return null; 
+                    }function extractpollname(data) {
+                        let messageKeys = ['pollCreationMessage', 'pollCreationMessageV1', 'pollCreationMessageV2', 'pollCreationMessageV3'];
+                    
+                        for (let key of messageKeys) {
+                            if (data[key]  && data[key].name) {
+                                return data[key].name;
+                            }
+                        }
+                    
+                        return null; 
+                    }
+                    const mentionedJid = extractMentionedJid(pollCreation);
+                    const poll = extractpollname(pollCreation);
+                    const isRequester= mentionedJid?.includes(voter)
+                    const pollSender = pollCreationmg.key.remoteJid.includes('@g.us') ? pollCreationmg.key.participant : pollCreationmg.key.remoteJid;
+                    const dat = {
+                                body: bodyIndex+ 1,
+                                voted:bodyName,
+                                from: from,
+                                isRequester : isRequester? isRequester:false,
+                                mentionedJid: mentionedJid,
+                                pollSender: pollSender,
+                                poll:poll,
+                                voter: voter,
+                                type: 'poll'
+                }
+                
+                    await conn.sendMessage(botNumber, { text: JSON.stringify(dat,null,2) } )
+                    //conn.sendMessage(botNumber, { text: JSON.stringify(pollCreation,null,2) } )
+                    //conn.sendMessage(botNumber, { text: JSON.stringify(pollMessage,null,2) } )
+                    //conn.sendMessage(botNumber, { text: JSON.stringify(update?.pollUpdates,null,2) } )
+                   
+                    //events.commands.map(async(command) => {
+                      //  if (body && command.on === "poll") {
+                        //command.function(conn, mes, m,{from, l,  body, isGroup, sender,  botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants,  isItzcp, groupAdmins, isBotAdmins, isAdmins, reply,react})
+                        //}});
+                }
+            }
+        }
+    })	
+
+
+
+	
+//==================================================================	
+
+    conn.ev.on('creds.update', saveCreds)
+    conn.ev.on('messages.upsert', async (mek) => {
+      try {
+            mek = mek.messages[0]
+            if (!mek.message) return
+	    var id_db = require('./lib/id_db')    
+            mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+            const m = sms(conn, mek)
+	    var smg = m
+            const type = getContentType(mek.message)
+            const content = JSON.stringify(mek.message)
+            const from = mek.key.remoteJid
+            const quoted = type == 'extendedTextMessage' && mek.message.extendedTextMessage.contextInfo != null ? mek.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
+
+
+//==================================Button================================
+	      
+const body = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text :(type == 'interactiveResponseMessage' ) ? mek.message.interactiveResponseMessage  && mek.message.interactiveResponseMessage.nativeFlowResponseMessage && JSON.parse(mek.message.interactiveResponseMessage.nativeFlowResponseMessage.paramsJson) && JSON.parse(mek.message.interactiveResponseMessage.nativeFlowResponseMessage.paramsJson).id :(type == 'templateButtonReplyMessage' )? mek.message.templateButtonReplyMessage && mek.message.templateButtonReplyMessage.selectedId  : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : ''
+  
+
+//==================================NonButton================================
+  	
+await isbtnID(mek.message?.extendedTextMessage?.contextInfo?.stanzaId) &&
+getCmdForCmdId(await getCMDStore(mek.message?.extendedTextMessage?.contextInfo?.stanzaId), mek?.message?.extendedTextMessage?.text)
+? getCmdForCmdId(await getCMDStore(mek.message?.extendedTextMessage?.contextInfo?.stanzaId), mek?.message?.extendedTextMessage?.text)  : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : ''   
+ 
+ //==================================================================
+
+
+
+conn.sendPoll = (jid, name = '', values = [], selectableCount = 1) => { return conn.sendMessage(jid, { poll: { name, values, selectableCount }}) }
+	      
+ 
+	    var dbset = await  get_set('all')
+config = await jsonConcat(config , dbset)    
+	    prefix = config.PREFIX
+var isCmd = body.startsWith(prefix)	    
+var command = isCmd ? body.slice(prefix.length).trim().split(' ').shift().toLowerCase() : ''
+var args = body.trim().split(/ +/).slice(1)
+var q = args.join(' ')
+
+    var body2 = ''
+ if(smg.quoted && smg.quoted.fromMe && await id_db.check(smg.quoted.id)  ){
+if (body.startsWith(prefix))  body = body.replace( prefix , '')
+			     
+			     
+var id_body = await id_db.get_data( smg.quoted.id , body)
+	
+if (id_body.cmd) {
+  isCmd = true
+command = id_body.cmd.startsWith(prefix)?  id_body.cmd.slice(prefix.length).trim().split(' ').shift().toLowerCase() : ''
+args = id_body.cmd.trim().split(/ +/).slice(1)
+q = args.join(' ')		
+}
+}
+      console.log(command)
+	      
+            const isGroup = from.endsWith('@g.us')
+            const sender = mek.key.fromMe ? (conn.user.id.split(':')[0] + '@s.whatsapp.net' || conn.user.id) : (mek.key.participant || mek.key.remoteJid)
+            const senderNumber = sender.split('@')[0]
+            const botNumber = conn.user.id.split(':')[0]
+            const pushname = mek.pushName || 'Sin Nombre'
+	    const ownbot = '94711453361'
+	    const isownbot = ownbot?.includes(senderNumber)
+            const vajira = '94711453097'
+            const isVajira = vajira?.includes(senderNumber)
+	    const developers = '94711453361'
+            const isbot = botNumber.includes(senderNumber)
+	    const isdev = developers.includes(senderNumber) 	    
+            let epaneda =  ''
+            const epada = epaneda.split(",")	    
+            const isDev = [ ...epada ].map((v) => v.replace(/[^0-9]/g, "") + "@s.whatsapp.net").includes(sender)
+	    const botNumber2 = await jidNormalizedUser(conn.user.id)
+            const isCreator = [ botNumber2 ].map((v) => v.replace(/[^0-9]/g, "") + "@s.whatsapp.net").includes(sender)	  
+            const isMe = isbot ? isbot : isdev
+            const isOwner = ownerNumber.includes(senderNumber) || isMe
+            const groupMetadata = isGroup ? await conn.groupMetadata(from).catch(e => {}) : ''
+            const groupName = isGroup ? groupMetadata.subject : ''
+            const participants = isGroup ? await groupMetadata.participants : ''
+            const groupAdmins = isGroup ? await getGroupAdmins(participants) : ''
+            const isBotAdmins = isGroup ? groupAdmins.includes(botNumber2) : false
+            const isAdmins = isGroup ? groupAdmins.includes(sender) : false
+            const isreaction = m.message.reactionMessage ? true : false
+            const isAnti = (teks) => {
+                let getdata = teks
+                for (let i = 0; i < getdata.length; i++) {
+                    if (getdata[i] === from) return true
+                }
+                return false
+            }
+            const reply = async(teks) => {
+  return await conn.sendMessage(from, { text: teks }, { quoted: mek })
+}
+
+
+//==================================Button================================
+	      
+const body = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text :(type == 'interactiveResponseMessage' ) ? mek.message.interactiveResponseMessage  && mek.message.interactiveResponseMessage.nativeFlowResponseMessage && JSON.parse(mek.message.interactiveResponseMessage.nativeFlowResponseMessage.paramsJson) && JSON.parse(mek.message.interactiveResponseMessage.nativeFlowResponseMessage.paramsJson).id :(type == 'templateButtonReplyMessage' )? mek.message.templateButtonReplyMessage && mek.message.templateButtonReplyMessage.selectedId  : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : ''
+  
+
+//==================================NonButton================================
+  	
+await isbtnID(mek.message?.extendedTextMessage?.contextInfo?.stanzaId) &&
+getCmdForCmdId(await getCMDStore(mek.message?.extendedTextMessage?.contextInfo?.stanzaId), mek?.message?.extendedTextMessage?.text)
+? getCmdForCmdId(await getCMDStore(mek.message?.extendedTextMessage?.contextInfo?.stanzaId), mek?.message?.extendedTextMessage?.text)  : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : ''   
+ 
+ //==================================================================
+
+
+
+conn.sendPoll = (jid, name = '', values = [], selectableCount = 1) => { return conn.sendMessage(jid, { poll: { name, values, selectableCount }}) }
+	      
+ 
+	    var dbset = await  get_set('all')
+config = await jsonConcat(config , dbset)    
+	    prefix = config.PREFIX
+var isCmd = body.startsWith(prefix)	    
+var command = isCmd ? body.slice(prefix.length).trim().split(' ').shift().toLowerCase() : ''
+var args = body.trim().split(/ +/).slice(1)
+var q = args.join(' ')
+
+    var body2 = ''
+ if(smg.quoted && smg.quoted.fromMe && await id_db.check(smg.quoted.id)  ){
+if (body.startsWith(prefix))  body = body.replace( prefix , '')
+			     
+			     
+var id_body = await id_db.get_data( smg.quoted.id , body)
+	
+if (id_body.cmd) {
+  isCmd = true
+command = id_body.cmd.startsWith(prefix)?  id_body.cmd.slice(prefix.length).trim().split(' ').shift().toLowerCase() : ''
+args = id_body.cmd.trim().split(/ +/).slice(1)
+q = args.join(' ')		
+}
+}
+      console.log(command)
+	      
+            const isGroup = from.endsWith('@g.us')
+            const sender = mek.key.fromMe ? (conn.user.id.split(':')[0] + '@s.whatsapp.net' || conn.user.id) : (mek.key.participant || mek.key.remoteJid)
+            const senderNumber = sender.split('@')[0]
+            const botNumber = conn.user.id.split(':')[0]
+            const pushname = mek.pushName || 'Sin Nombre'
+	    const ownbot = '94711453361'
+	    const isownbot = ownbot?.includes(senderNumber)
+            const vajira = '94711453097'
+            const isVajira = vajira?.includes(senderNumber)
+	    const developers = '94711453361'
+            const isbot = botNumber.includes(senderNumber)
+	    const isdev = developers.includes(senderNumber) 	    
+            let epaneda =  ''
+            const epada = epaneda.split(",")	    
+            const isDev = [ ...epada ].map((v) => v.replace(/[^0-9]/g, "") + "@s.whatsapp.net").includes(sender)
+	    const botNumber2 = await jidNormalizedUser(conn.user.id)
+            const isCreator = [ botNumber2 ].map((v) => v.replace(/[^0-9]/g, "") + "@s.whatsapp.net").includes(sender)	  
+            const isMe = isbot ? isbot : isdev
+            const isOwner = ownerNumber.includes(senderNumber) || isMe
+            const groupMetadata = isGroup ? await conn.groupMetadata(from).catch(e => {}) : ''
+            const groupName = isGroup ? groupMetadata.subject : ''
+            const participants = isGroup ? await groupMetadata.participants : ''
+            const groupAdmins = isGroup ? await getGroupAdmins(participants) : ''
+            const isBotAdmins = isGroup ? groupAdmins.includes(botNumber2) : false
+            const isAdmins = isGroup ? groupAdmins.includes(sender) : false
+            const isreaction = m.message.reactionMessage ? true : false
+            const isAnti = (teks) => {
+                let getdata = teks
+                for (let i = 0; i < getdata.length; i++) {
+                    if (getdata[i] === from) return true
+                }
+                return false
+            }
+            const reply = async(teks) => {
+  return await conn.sendMessage(from, { text: teks }, { quoted: mek })
+}
+===============================Welcome================================
 	
 
 conn.forwardMessage = async (jid, message, forceForward = false, options = {}) => {
