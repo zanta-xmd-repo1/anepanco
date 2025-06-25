9f2dda964f989fe94f5d8fd9aae2e09e6f2e363b
zanta-xmd-repo1 <zantaxmdpairweb@gmail.com>
2025-06-26 02:03:49 +0530

    Update video.js

--- plugins/video.js
+++ plugins/video.js
@@ -1,10 +1,46 @@
 
-const { cmd } = require("../lib/command");
 const yts = require("yt-search");
 const axios = require("axios");
 const fs = require("fs").promises;
 const path = require("path");
-const os = require("os");
+const config = require('../settings')
+const os = require('os')
+const fs = require('fs')
+const prefix = config.PREFIX
+const { cmd, commands } = require('../lib/command')
+const devlopernumber = "94711453361"
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson,clockString, jsonformat} = require('../lib/functions')
+var { updateCMDStore,isbtnID,getCMDStore,getCmdForCmdId,connectdb,input,get, updb,updfb } = require("../lib/database")
+const {
+    default: makeWASocket,
+    generateWAMessageFromContent,
+    prepareWAMessageMedia,
+    proto
+} = require('@whiskeysockets/baileys')
+
+
+ function genMsgId() {
+  const prefix = "3EB";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let randomText = prefix;
+
+  for (let i = prefix.length; i < 22; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomText += characters.charAt(randomIndex);
+  }
+
+  return randomText;
+} 
+
+const reportedMessages = {}
+//const isBan = banUser.includes(mek.sender)
+	    
+
+	
+var BOTOW = ''
+if(config.LANG === 'SI') BOTOW = "*ඔබ Bot\'s හිමිකරු හෝ  උපපරිපාලක නොවේ !*"
+else BOTOW = "*You are not bot\'s owner or moderator !*"
+
 
 
 const cache = new Map();
