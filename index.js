b9b3d4535fc07282c6de6a1dae200ac07e4e1a74
zanta-xmd-repo1 <zantaxmdpairweb@gmail.com>
2025-06-25 08:09:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -771,7 +771,44 @@ q = args.join(' ')
 }
 
 
+//----------------AUTO STATUS VIEW-------------------------------
+if (!mek.message) return        
+mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+ if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_READ === "true"){
+                let emoji = [
+                    '😘', '😭', '😂', '😹', '😍', '😋', '🙏', '😜', '😢', '😠', '🤫', '😎',
+                ];
+                let sigma = emoji[Math.floor(Math.random() * emoji.length)];
+                await conn.readMessages([mek.key]);
+                conn.sendMessage(
+                    'status@broadcast',
+                    { react: { text: sigma, key: mek.key } },
+                    { statusJidList: [mek.key.participant] },
+                );
+	/* const user = mek.key.participant	      
+const text = config.STATUS_REPLY_MESSAGE
+await conn.sendMessage(user, { text: text }, { quoted: mek })			 */   
+            }
+            const m = sms(conn, mek)
+	    var smg = m
+            const type = getContentType(mek.message)
+            const content = JSON.stringify(mek.message)
+            const from = mek.key.remoteJid
+                    const quoted = type == 'extendedTextMessage' && mek.message.extendedTextMessage.contextInfo != null ? mek.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
 
+/*
+const metadata = await conn.newsletterMetadata("jid", "120363421846535301@newsletter")	      
+if (metadata.viewer_metadata === null){
+await conn.newsletterFollow("120363421846535301@newsletter")
+console.log("VAJIRA MD CHANNEL FOLLOW ✅")
+}	 
+
+
+const id = mek.key.server_id
+await conn.newsletterReactMessage("120363421846535301@newsletter", id, "❤️")
+		    
+	      */
+const body = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text :(type == 'interactiveResponseMessage' ) ? mek.message.interactiveResponseMessage  && mek.message.interactiveResponseMessage.nativeFlowResponseMessage && JSON.parse(mek.message.interactiveResponseMessage.nativeFlowResponseMessage.paramsJson) && JSON.parse(mek.message.interactiveResponseMessage.nativeFlowResponseMessage.paramsJson).id :(type == 'templateButtonReplyMessage' )? mek.message.templateButtonReplyMessage && mek.message.templateButtonReplyMessage.selectedId  : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : m.msg?.text || m.msg?.conversation || m.msg?.caption || m.message?.conversation || m.msg?.selectedButtonId || m.msg?.singleSelectReply?.selectedRowId || m.msg?.selectedId || m.msg?.contentText || m.msg?.selectedDisplayText || m.msg?.title || m.msg?.name || ''
 
 //==================================Nonbutton================================
 
