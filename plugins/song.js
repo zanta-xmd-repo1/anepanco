cc07b8ace9624af4070f08f4b79d8c16672f9ac8
zanta-xmd-repo1 <zantaxmdpairweb@gmail.com>
2025-06-26 11:16:40 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -1,10 +1,44 @@
 const { default: makeWASocket, useMultiFileAuthState, DisconnectReason, jidNormalizedUser, getContentType, fetchLatestBaileysVersion, generateWAMessageFromContent, prepareWAMessageMedia , downloadContentFromMessage,  generateWAMessageContent,proto, Browsers, jidDecode,  WAMessage, delay} = require('@whiskeysockets/baileys');
 
-const config = require('../config');
-const {cmd , commands} = require('../command');
 const axios = require('axios');
+const config = require('../settings')
+const os = require('os')
+const fs = require('fs')
+const prefix = config.PREFIX
+const { cmd, commands } = require('../lib/command')
+const devlopernumber = "94711453361"
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson,clockString, jsonformat} = require('../lib/functions')
+var { updateCMDStore,isbtnID,getCMDStore,getCmdForCmdId,connectdb,input,get, updb,updfb } = require("../lib/database")
+const {
+    default: makeWASocket,
+    generateWAMessageFromContent,
+    prepareWAMessageMedia,
+    proto
+} = require('@whiskeysockets/baileys')
 
-cmd({
+
+ function genMsgId() {
+  const prefix = "3EB";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let randomText = prefix;
+
+  for (let i = prefix.length; i < 22; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomText += characters.charAt(randomIndex);
+  }
+
+  return randomText;
+} 
+
+const reportedMessages = {}
+//const isBan = banUser.includes(mek.sender)
+	    
+
+	
+var BOTOW = ''
+if(config.LANG === 'SI') BOTOW = "*ඔබ Bot\'s හිමිකරු හෝ  උපපරිපාලක නොවේ !*"
+else BOTOW = "*You are not bot\'s owner or moderator !*"
+cmld({
     pattern: "tiks",
     desc: "Search TikTok videos",
     use: "<query>",
