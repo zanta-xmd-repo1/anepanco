7e7ebb4a106596d157d08fc0f1ea3fe47edf8b8d
zanta-xmd-repo1 <zantaxmdpairweb@gmail.com>
2025-06-29 21:14:17 +0530

    Create menu2.js

---
+++ plugins/menu2.js
@@ -0,0 +1,133 @@
+cmd({
+    pattern: "menu2",
+    react: "📂",
+    alias: ["help"],
+    desc: "Get bot\'s command list.",
+    category: "main",
+    use: '.menu',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname,  isSachintha, isSavi, isSadas, isMani, isMe,isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+    try{
+    let menuc1 = ``
+for (let i=0;i<commands.length;i++) { 
+if(commands[i].category === 'download'){
+if(!commands[i].dontAddCommandList){
+menuc1 += `*│►* .${commands[i].pattern}\n`
+}}};
+
+let menuc2 = ``
+for (let i=0;i<commands.length;i++) { 
+  if(commands[i].category === 'search'){
+  if(!commands[i].dontAddCommandList){
+  menuc2 += `*│⩥* .${commands[i].pattern}\n`
+  }}};
+
+let menuc3 = ``
+for (let i=0;i<commands.length;i++) { 
+if(commands[i].category === 'convert'){
+  if(!commands[i].dontAddCommandList){
+    menuc3 += `*│►* .${commands[i].pattern}\n`
+}}};
+
+let menuc4 = ``
+for (let i=0;i<commands.length;i++) { 
+if(commands[i].category === 'logo'){
+  if(!commands[i].dontAddCommandList){
+menuc4 += `*│►* .${commands[i].pattern}\n`
+}}};
+
+let menuc5 = ``
+for (let i=0;i<commands.length;i++) { 
+if(commands[i].category === 'main'){
+  if(!commands[i].dontAddCommandList){
+menuc5 += `*│►* .${commands[i].pattern}\n`
+}}};
+
+let menuc6 = ``
+for (let i=0;i<commands.length;i++) { 
+if(commands[i].category === 'group'){
+if(!commands[i].dontAddCommandList){
+  menuc6 += `*│⩥* .${commands[i].pattern}\n`
+}}};
+
+let menuc7 = ``
+for (let i=0;i<commands.length;i++) { 
+if(commands[i].category === 'bug'){
+if(!commands[i].dontAddCommandList){
+  menuc7 += `*│⩥* .${commands[i].pattern}\n`
+}}};	
+
+let menuc8 = ``
+for (let i=0;i<commands.length;i++) { 
+if(commands[i].category === 'other'){
+if(!commands[i].dontAddCommandList){
+  menuc8 += `*│⩥* .${commands[i].pattern}\n`
+}}};
+
+let menuc9 = ``
+for (let i=0;i<commands.length;i++) { 
+if(commands[i].category === 'movie'){
+if(!commands[i].dontAddCommandList){
+  menuc9 += `*│⩥* .${commands[i].pattern}\n`
+}}};
+     
+let menumg = `*Hellow👸* ${pushname}
+
+*╭─     ᴄᴏᴍᴍᴀɴᴅꜱ ᴘᴀɴᴇʟ*
+*│🕵️‍♂️ 𝘙𝘶𝘯 𝘛𝘪𝘮𝘦 -* ${runtime(process.uptime())} 
+*│🕵️‍♂️ 𝘙𝘢𝘮 𝘜𝘴𝘦 -* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*╰──────────●●►*
+*👸 𝘝𝘈𝘑𝘐𝘙𝘈 𝘔𝘥 𝘊𝘰𝘮𝘮𝘢𝘮𝘥 𝘗𝘢𝘯𝘦𝘭*
+*╭──────────●●►*
+*│🧙‍♂️ DOWNLOAD COMMANDS*
+*│   ───────*
+
+${menuc1}*╰───────────●●►*
+*╭──────────●●►*
+*│🧙‍♂️ SEARCH COMMANDS*
+*│   ───────*
+
+${menuc2}*╰───────────●●►*
+
+*╭──────────●●►*
+*│🧙‍♂️ CONVERT COMMANDS*
+*│   ───────*
+
+${menuc3}*╰───────────●●►*
+
+*╭──────────●●►*
+*│🧙‍♂️ LOGO COMMANDS*
+*│   ───────*
+
+${menuc4}*╰───────────●●►*
+
+*╭──────────●●►*
+*│🧙‍♂️ MAIN COMMANDS*
+*│   ───────*
+
+${menuc5}*╰───────────●●►*
+
+*╭──────────●●►*
+*│🧙‍♂️ GROUP COMMANDS*
+*│   ───────*
+
+${menuc6}*╰───────────●●►*
+		       
+*╭──────────●●►*
+*│🧙‍♂️ BUG COMMANDS*
+*│   ───────*
+
+${menuc7}*╰───────────●●►*	
+
+*╭──────────●●►*
+*│🧙‍♂️ OTHER COMMANDS*
+*│   ───────*
+
+${menuc8}*╰───────────●●►*	
+
+*╭──────────●●►*
+*│🧙‍♂️ MOVIE COMMANDS*
+*│   ───────*
+
+${menuc9}*╰───────────●●►*	
