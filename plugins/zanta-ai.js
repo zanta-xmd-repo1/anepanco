c442c2a0d89b5f0d8af42d48a520d663019ab61c
zanta-xmd-repo1 <zantaxmdpairweb@gmail.com>
2025-06-29 21:40:33 +0530

    Create zanta-ai.js

---
+++ plugins/zanta-ai.js
@@ -0,0 +1,105 @@
+const { cmd } = require("../command");
+const axios = require("axios");
+const fs = require("fs");
+
+cmd({
+  pattern: "fluxai2",
+  alias: ["aiflux2", "imagine2"],
+  react: "🚀",
+  desc: "Generate an image using AI.",
+  category: "main",
+  filename: __filename
+}, async (conn, mek, m, { q, reply }) => {
+  try {
+    if (!q) return reply("Please provide a prompt for the image.");
+
+    await reply("> *CREATING IMAGINE ...🔥*");
+
+    const apiUrl = `https://api.siputzx.my.id/api/ai/flux?prompt=${encodeURIComponent(q)}`;
+
+    const response = await axios.get(apiUrl, { responseType: "arraybuffer" });
+
+    if (!response || !response.data) {
+      return reply("Error: The API did not return a valid image. Try again later.");
+    }
+
+    const imageBuffer = Buffer.from(response.data, "binary");
+
+    await conn.sendMessage(m.chat, {
+      image: imageBuffer,
+      caption: `✨ Prompt: *${q}\n>  *𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐲 - : 𝐌𝐑 𝐒𝐔𝐑𝐀𝐍𝐆𝐀 𝐎𝐅𝐂 💖*`
+    });
+
+  } catch (error) {
+    console.error("FluxAI Error:", error);
+    reply(`An error occurred: ${error.response?.data?.message || error.message || "Unknown error"}`);
+  }
+});
+
+cmd({
+  pattern: "stablsdssediffusion",
+  alias: ["sdiffussssdsion", "imagine3"],
+  react: "🚀",
+  desc: "Generate an image using AI.",
+  category: "main",
+  filename: __filename
+}, async (conn, mek, m, { q, reply }) => {
+  try {
+    if (!q) return reply("Please provide a prompt for the image.");
+
+    await reply("> *CREATING IMAGINE ...🔥*");
+
+    const apiUrl = `https://api.siputzx.my.id/api/ai/stable-diffusion?prompt=${encodeURIComponent(q)}`;
+
+    const response = await axios.get(apiUrl, { responseType: "arraybuffer" });
+
+    if (!response || !response.data) {
+      return reply("Error: The API did not return a valid image. Try again later.");
+    }
+
+    const imageBuffer = Buffer.from(response.data, "binary");
+
+    await conn.sendMessage(m.chat, {
+      image: imageBuffer,
+      caption: `✨ Prompt: *${q}*\n>  *𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐲 - : 𝐌𝐑 𝐒𝐔𝐑𝐀𝐍𝐆𝐀 𝐎𝐅𝐂 💖*`
+    });
+
+  } catch (error) {
+    console.error("FluxAI Error:", error);
+    reply(`An error occurred: ${error.response?.data?.message || error.message || "Unknown error"}`);
+  }
+});
+
+cmd({
+  pattern: "stabiwewelityai",
+  alias: ["stabilwwewewity", "imagine4"],
+  react: "🚀",
+  desc: "Generate an image using AI.",
+  category: "main",
+  filename: __filename
+}, async (conn, mek, m, { q, reply }) => {
+  try {
+    if (!q) return reply("Please provide a prompt for the image.");
+
+    await reply("> *CREATING IMAGINE ...🔥*");
+
+    const apiUrl = `https://api.siputzx.my.id/api/ai/stabilityai?prompt=${encodeURIComponent(q)}`;
+
+    const response = await axios.get(apiUrl, { responseType: "arraybuffer" });
+
+    if (!response || !response.data) {
+      return reply("Error: The API did not return a valid image. Try again later.");
+    }
+
+    const imageBuffer = Buffer.from(response.data, "binary");
+
+    await conn.sendMessage(m.chat, {
+      image: imageBuffer,
+      caption: `✨ Prompt: *${q}*\n>  *𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐲 - : 𝐌𝐑 𝐒𝐔𝐑𝐀𝐍𝐆𝐀 𝐎𝐅𝐂 💖*`
+    });
+
+  } catch (error) {
+    console.error("api Error:", error);
+    reply(`An error occurred: ${error.response?.data?.message || error.message || "Unknown error"}`);
+  }
+});
